6762a5e8c222f8990e8acfca8ce811877e88162d
lordish <gitonga403@gmail.com>
2023-03-19 18:36:47 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+]
+
+const superBowlWin = record.find(record => record.year==="2015");
+console.log(superBowlWin);
+
